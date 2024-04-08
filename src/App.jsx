9cc07b6ca2193752b64ab9fe2d82cd3c81e9d135
s0raya <soraya.alcobet@gmail.com>
2024-04-08 22:11:29 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
+import useFetchCharacters from './hooks/useFecthCharacters';
 
 function App() {
   const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
   const urlRick = 'https://rickandmortyapi.com/api/character/1';
-  
+
+  const pokemonData = useFetchCharacters(urlPokemon);
+  const rickAndMortyData = useFetchCharacters(urlRick);
+
   return (
     <>
+      <h2>Pokemon</h2>
+      <p>{pokemonData.nameCharacter}</p>
+      <img src={pokemonData.imgCharacter} alt={pokemonData.nameCharacter} />
+
+      <h2>Rick And Morty</h2>
+      <p>{rickAndMortyData.nameCharacter}</p>
+      <img src={rickAndMortyData.imgCharacter} alt={rickAndMortyData.nameCharacter} />
     </>
   );
 }
---
+++ src/hooks/useFecthCharacters.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+
+
+const useFetchCharacters = (url) => {
+    const [nameCharacter, setNameCharacter] = useState(null);
+    const [imgCharacter, setImgCharacter ] = useState('');
+    const [error, setError] = useState('');
+
+    useEffect(() => {    
+        const functionFetch = async () => {
+            try {
+                const response = await fetch(url);                
+                if (!response.ok) throw new Error('Error al obtener los personajes');
+                const data = await response.json();
+                setNameCharacter(data.name);
+                if(url.includes('pokeapi')) {setImgCharacter(data.sprites.front_default)}
+                else {setImgCharacter(data.image)};
+            } catch (error) {
+                setError(error.message);
+            }
+        }
+        functionFetch();
+    }, []);   
+    return { nameCharacter, imgCharacter, error };
+}
+
+export default useFetchCharacters;
